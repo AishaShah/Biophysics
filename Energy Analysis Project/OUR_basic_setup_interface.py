702bea9a13bf9ac8cc68f8472c172b2026467aeb
Aisha Shah <aisha.shah@alum.esci.upf.edu>
2021-11-28 15:23:05 +0100

    Update OUR_basic_setup_interface.py

--- Energy Analysis Project/OUR_basic_setup_interface.py
+++ Energy Analysis Project/OUR_basic_setup_interface.py
@@ -35,12 +35,17 @@ parser.add_argument(
 
 parser.add_argument('pdb_file', help='Input PDB', type=open)
 parser.add_argument('pdbqt_file', help='Input PDBQT', type=open)
+parser.add_argument('ChainA_pdb', help='Input PDB for chain A', type=open)
+parser.add_argument('ChainB_pdb', help='Input PDB for chain B', type=open)
+parser.add_argument('cutoff', type=float, help='cut-off distance')
+
 
 args = parser.parse_args()
 
 print("PDB File:", args.pdb_file.name)
 print("PDBQT File:", args.pdbqt_file.name)
 print("Parameters File:", args.vdwprm_file)
+print('Cut-off:', args.cutoff)
 
 # loading VdW parameters
 ff_params = VdwParamset(args.vdwprm_file)
@@ -50,6 +55,8 @@ print('Parsing PDB', args.pdb_file.name)
 
 # load structure from PDB file of PDB ifle handler
 st = parser.get_structure('STR', args.pdb_file.name)
+st_A = parser.get_structure('STR', args.ChainA_pdb.name)
+st_B = parser.get_structure('STR', args.ChainB_pdb.name)
 
 # We will use the xtra attribute in Bio.PDB.Atom to hold the new data
 # Getting Charges and Atom type from PDBQT
@@ -73,8 +80,6 @@ total_charge = 0.
 for at in st.get_atoms():
     at.xtra['atom_type'] = params[at.serial_number]['type']
     at.xtra['charge'] = float(params[at.serial_number]['charge'])
-    print(at, at.get_parent())
-
     at.xtra['vdw'] = ff_params.at_types[at.xtra['atom_type']]
     total_charge += at.xtra['charge']
 print('Total Charge: {:8.2f}'.format(total_charge))
@@ -82,54 +87,5 @@ print('Total Charge: {:8.2f}'.format(total_charge))
 # Calculating surfaces
 # Srf goes to .xtra['EXP_NACCESS'] field
 srf = NACCESS_atomic(st[0], naccess_binary=args.naccess_bin)
-
-# Simple Test for atom fields. Choose one atom from the PDB file. st[model][chain_id][res_number][atom_name]
-'''
-print(vars(st[0]['A'][42]['N']))
-print('')
-print(vars(st[0]['A'][42]['N'].xtra['vdw']))
-
-# Getting this is ASA
-print((st[0]['A'][42]['N'].xtra['EXP_NACCESS']))
-# Getting fsrf that is sigma for solvation
-print((st[0]['A'][42]['N'].xtra['vdw']).fsrf)
-'''
-
-
-def solv_energy(st, residues_list):
-    solv_energies = {}
-    for res in residues_list:
-        delG_solv = 0
-        for atom in res.get_atoms():
-            # calculate solvation of each atom
-            # get value of sigma and ASA
-            if 'EXP_NACCESS' in atom.xtra:
-                delG_solv += atom.xtra['vdw'].fsrf * \
-                    float(atom.xtra['EXP_NACCESS'])
-        solv_energies[res.id[1]] = delG_solv
-    return solv_energies
-
-
-'''
-print('CHAIN A')
-print(solv_energy(st, resA))
-print('CHAIN E')
-print(solv_energy(st, resE))
-'''
-
-
-def solv_energy(st):
-    solv_energies = {}
-    for res in st.get_residues():
-        delG_solv = 0
-        for atom in res.get_atoms():
-            # calculate solvation of each atom
-            # get value of sigma and ASA
-            if 'EXP_NACCESS' in atom.xtra:
-                delG_solv += atom.xtra['vdw'].fsrf * \
-                    float(atom.xtra['EXP_NACCESS'])
-        solv_energies[res.id[1]] = delG_solv
-    return solv_energies
-
-
-print(solv_energy(st))
+srf_A = NACCESS_atomic(st_A[0], naccess_binary=args.naccess_bin)
+srf_B = NACCESS_atomic(st_B[0], naccess_binary=args.naccess_bin)
